503d2ac114399837a350fe887ff03d2f47a72972
Chris Nielubowicz <cnielubowicz@mobiquityinc.com>
2015-09-03 17:43:40 -0400

    Initial Commit

---
+++ ColorProvider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorProvider
+//
+//  Created by Chris Nielubowicz on 9/3/15.
+//  Copyright © 2015 Mobiquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
